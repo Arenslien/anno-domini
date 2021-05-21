b802bdf7ec080a9f7deb21d23702ecd962bd2d50
Arenslien <arenslien@gmail.com>
2021-05-21 23:27:21 +0900

    bibler & web server post request 구현 중

--- calendar/lib/main.dart
+++ calendar/lib/main.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:calendar/screens/wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-
 void main() async {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
